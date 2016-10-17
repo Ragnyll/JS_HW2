51ee923962e812aa8633f15b059532f6b472d81a
Ragnyll <justanotherjakeyg@gmail.com>
2016-10-17 14:40:15 -0500

    remind me now functions as specified

--- scripts/remind-me.js
+++ scripts/remind-me.js
@@ -26,6 +26,6 @@ module.exports = function(robot) {
         // > Don't forget to <task>!
         setTimeout(function() {
             msg.send("Don\'t forget to ", task, "!");
-        }, numSeconds);
+        }, numSeconds * 1000);
     });
 };
