5c0b93b83cffe328d4ae934da2a4871a108f89e1
Ragnyll <justanotherjakeyg@gmail.com>
2016-10-19 10:42:57 -0500

    The retrieve todos is buggy right before here 3

--- scripts/todos.js
+++ scripts/todos.js
@@ -27,90 +27,115 @@ const DATA_DIR = 'todos';
 
 module.exports = function(robot) {
 
-  // Check to see if our chosen DATA_DIR exists. If it doesn't, we'll
-  // make it.
-  try {
-    fs.statSync(DATA_DIR);
-  } catch (error) {
-    fs.mkdirSync(DATA_DIR);
-  }
+    // Check to see if our chosen DATA_DIR exists. If it doesn't, we'll
+    // make it.
+    try {
+        fs.statSync(DATA_DIR);
+    } catch (error) {
+        fs.mkdirSync(DATA_DIR);
+    }
 
-  // Handler for 'hubot show my todo list'
-  robot.respond(/show my todo list$/i, function(msg) {
+    // Handler for 'hubot show my todo list'
+    robot.respond(/show my todo list$/i, function(msg) {
 
-    // Uses `fs.readdir` to list the names of the files in the
-    // `DATA_DIR` directory.
+        // Uses `fs.readdir` to list the names of the files in the
+        // `DATA_DIR` directory.
 
-    // If the `fs.readdir` callback receives an error (if the error
-    // exists), then the hubot will reply with the following:
+        // If the `fs.readdir` callback receives an error (if the error
+        // exists), then the hubot will reply with the following:
 
-    // > Oh no... I couldn't look for todos...
+        // > Oh no... I couldn't look for todos...
 
-    // Otherwise, if the list of todo files is empty, the hubot will
-    // reply with:
+        // Otherwise, if the list of todo files is empty, the hubot will
+        // reply with:
 
-    // > The list is empty!
+        // > The list is empty!
 
-    // Otherwise, we loop over the file names, using `fs.readFile` to
-    // read the contents of each file. We will need to insert a closure
-    // wrapper in order to ensure that loop variables are closed over.
+        // Otherwise, we loop over the file names, using `fs.readFile` to
+        // read the contents of each file. We will need to insert a closure
+        // wrapper in order to ensure that loop variables are closed over.
 
-    // Whenever a file is read, its corresponding callback will be
-    // executed. Each `fs.readFile` callback takes two parameters:
-    // `error` and `data`.
+        // Whenever a file is read, its corresponding callback will be
+        // executed. Each `fs.readFile` callback takes two parameters:
+        // `error` and `data`.
 
-    // If `error` exists, then the hubot will reply with:
+        // If `error` exists, then the hubot will reply with:
 
-    // > Uh oh! Had trouble opening a todo...
+        // > Uh oh! Had trouble opening a todo...
 
-    // Otherwise, the hubot will reply with the ID of the todo item (its
-    // file name) and the content of the todo file:
+        // Otherwise, the hubot will reply with the ID of the todo item (its
+        // file name) and the content of the todo file:
 
-    // > <itemID>: <item>
+        // > <itemID>: <item>
 
-    // In summary, for each file in the `DATA_DIR` directory, we call
-    // `fs.readFile` and register a callback, so that when the content of
-    // a todo file is ready, we can reply to the user.
+        // In summary, for each file in the `DATA_DIR` directory, we call
+        // `fs.readFile` and register a callback, so that when the content of
+        // a todo file is ready, we can reply to the user.
 
-    // We don't necessarily know the order of the files in the directory,
-    // nor do we know how long it will take to read each file, so the
-    // hubot may reply with todo items in a totally arbitrary order.
+        // We don't necessarily know the order of the files in the directory,
+        // nor do we know how long it will take to read each file, so the
+        // hubot may reply with todo items in a totally arbitrary order.
+        fs.readdir(DATA_DIR, function(err, files) {
+            if (err) {
+                msg.reply("Oh no... I couldn't look for todos...");
+            } else if (files.length === 0) {
+                msg.reply("The list is empty!");
+            } else {
+                console.log("here1");
+                for (let file of files) {
+                    fs.readFile((DATA_DIR + '/' + file),
+                        function read(err, data) {
+                            content = data;
+                            console.log(data);
+                            console.log("here3");
+                            if (err) {
+                                console.log("here4");
+                                msg.reply("Uh oh! Had trouble opening a todo...");
+                            } else {
+                                console.log("here5");
+                                msg.reply("IVE GOT THE MESSAGE!")
+                            }
+                        });
+                }
 
-  });
+            }
+        });
 
-  // Handler for 'hubot add <item> to my todo list'
-  robot.respond(/add (.*) to my todo list$/i, function(msg) {
-    let todo = msg.match[1];
+    });
 
-    // Uses `fs.writeFile` to save the todo item (with ASCII encoding) to
-    // a file with a name computed with `uuid.v4`.
+    // Handler for 'hubot add <item> to my todo list'
+    robot.respond(/add (.*) to my todo list$/i, function(msg) {
+        let todo = msg.match[1];
 
-    // When Node is done writing the data to the specified file, it calls
-    // our callback. If there's an error, the hubot replies with:
+        // Uses `fs.writeFile` to save the todo item (with ASCII encoding) to
+        // a file with a name computed with `uuid.v4`.
 
-    // > Oh no... I couldn't write the todo file...
+        // When Node is done writing the data to the specified file, it calls
+        // our callback. If there's an error, the hubot replies with:
 
-    // Otherwise, the hubot replies with:
+        // > Oh no... I couldn't write the todo file...
 
-    // > OK! I added <todo> to the todo list
+        // Otherwise, the hubot replies with:
 
-    // Note that file names have no extensions. They are just UUIDs.
+        // > OK! I added <todo> to the todo list
 
-  });
+        // Note that file names have no extensions. They are just UUIDs.
 
-  // Handler for 'hubot <itemID> is done'
-  robot.respond(/([0-9a-f\-]{36}) is done$/i, function(msg) {
-    let id = msg.match[1];
+    });
 
-    // Uses `fs.unlink` to delete the todo file from `DATA_DIR` with name
-    // `id`. If there is an error deleting the file, the hubot replies
-    // with:
+    // Handler for 'hubot <itemID> is done'
+    robot.respond(/([0-9a-f\-]{36}) is done$/i, function(msg) {
+        let id = msg.match[1];
 
-    // > Couldn't find <id>
+        // Uses `fs.unlink` to delete the todo file from `DATA_DIR` with name
+        // `id`. If there is an error deleting the file, the hubot replies
+        // with:
 
-    // Otherwise, it replies with
+        // > Couldn't find <id>
 
-    // > OK! Removed <id>
+        // Otherwise, it replies with
 
-  });
+        // > OK! Removed <id>
+
+    });
 };
